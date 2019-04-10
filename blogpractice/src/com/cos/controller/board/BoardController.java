25c46b2a2d593feb933d4bbe0afc2ad1bc664b05
Jaeorin <47616192+jaeorin@users.noreply.github.com>
2019-04-10 16:31:59 +0900

    추가

--- blogpractice/src/com/cos/controller/board/BoardController.java
+++ blogpractice/src/com/cos/controller/board/BoardController.java
@@ -25,6 +25,7 @@ public class BoardController extends HttpServlet {
 			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF=8");
 		
 		DBManager.getConnection();
 		
